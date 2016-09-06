1e871dc1efe6241c2c05cc3dd0e12888a4798323
Chen Fang <chenfanggm@hotmail.com>
2016-09-05 19:24:01 -0700

    [clean] clean code structure

--- client/main.js
+++ client/main.js
@@ -2,50 +2,49 @@ import * as THREE from 'three'
 import config from '../config'
 import './styles/main.scss'
 
+
 const MODEL_POOL = {}
 const MAIN = {}
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer()
-const loader = new THREE.TextureLoader()
-const mainCanvas = document.getElementById('mainCanvas')
-/**
- * Init Renderer
- */
-const initRenderer = () => {
+const textureLoader = new THREE.TextureLoader()
+const canvasDom = document.getElementById('mainCanvas')
+
+const initialize = () => {
+  // camera
+  camera.position.z = 10
   // renderer
   renderer.setSize(window.innerWidth/1.5, window.innerHeight/1.5)
   renderer.setClearColor(0x3399ff)
-
   // model
   const geometry = new THREE.BoxGeometry(5, 5, 5)
   const material = new THREE.MeshLambertMaterial({color: 0xf6546a})
   MODEL_POOL.box = new THREE.Mesh(geometry, material)
-
   // texture
-  loader.load("./static/images/box_texture.jpg", (texture) => {
+  textureLoader.load("./static/images/box_texture.jpg", (texture) => {
     const material = new THREE.MeshLambertMaterial({map: texture})
     MODEL_POOL.box = new THREE.Mesh(geometry, material)
     scene.add(MODEL_POOL.box)
   })
-
   // light
   const light = new THREE.PointLight(0xffffff, 1.2)
-  light.position.set(0, 0, 6)
+  light.position.set(0, 0, 5)
   scene.add(light)
-
-  // camera
-  camera.position.z = 10
-
-  mainCanvas.appendChild(renderer.domElement)
+  // canvas DOM
+  canvasDom.appendChild(renderer.domElement)
 }
 
-const renderLoop = () => {
+const render = () => {
   MODEL_POOL.box.rotation.x += 0.1
   MODEL_POOL.box.rotation.y += 0.1
   renderer.render(scene, camera)
-  MAIN.runningLoop = requestAnimationFrame(renderLoop)
+}
+
+const animate = () => {
+  MAIN.runningLoop = requestAnimationFrame(animate)
+  render()
 }
 
 const reload = () => {
@@ -55,24 +54,30 @@ const reload = () => {
   }
 
   console.log('Removing all children...')
-  scene.children.map((obj) => {
-    scene.remove(obj)
-  })
-
-  if (mainCanvas.childNodes[0]) {
-    mainCanvas.removeChild(mainCanvas.childNodes[0])
+  if (scene.children) {
+    scene.children.map((obj) => {
+      scene.remove(obj)
+    })
   }
 
-  initRenderer()
-  MAIN.runningLoop = renderLoop()
+  if (Object.keys(MODEL_POOL)) {
+    Object.keys(MODEL_POOL).map((key) => {
+      scene.remove(MODEL_POOL[key])
+    })
+  }
+
+  if (canvasDom.childNodes[0]) {
+    canvasDom.removeChild(canvasDom.childNodes[0])
+  }
+
+  initialize()
+  MAIN.runningLoop = animate()
   console.log('Reload complete.')
 }
 
 reload()
 
-/**
- * HMR
- */
+// Hot module replace setting
 if (config.env === 'development') {
   if (module.hot) {
     module.hot.accept()
